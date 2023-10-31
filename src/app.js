577aadaea87b0184531a6e19b16c5a60f38f4108
ricardodelimaaa <142522306+ricardodelimaaa@users.noreply.github.com>
2023-10-31 02:27:04 +0000

    generadordominio

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let i;
+let b;
+let c;
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (i = 0; i < pronoun.length; i++) {
+  for (b = 0; b < adj.length; b++) {
+    for (c = 0; c < noun.length; c++) {
+      console.log(pronoun[i] + adj[b] + noun[c] + "puedes.es \n");
+    }
+  }
+}
